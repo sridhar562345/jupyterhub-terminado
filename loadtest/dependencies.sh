b8e33e305ca4ffe5621404e76014de0ea94ec91b
sridhar562345 <sridhar562345@gmail.com>
2020-12-07 17:21:43 +0530

    add loadtest

---
+++ loadtest/dependencies.sh
@@ -0,0 +1,10 @@
+sudo apt-get update
+
+sudo apt-get install python3-pip python3-venv
+
+python3 -m venv env
+
+. env/bin/activate
+
+pip3 install locust selenium
+
---
+++ loadtest/locust/locustfile.py
@@ -0,0 +1,30 @@
+import time
+from locust import HttpUser, task, between
+import json
+import requests
+
+class QuickstartUser(HttpUser):
+    wait_time = between(1, 2)
+
+    @task
+    def index_page(self):
+        # self.client.get("/")
+        from selenium import webdriver
+        from selenium.webdriver.common.keys import Keys
+        from selenium.webdriver.common.by import By
+        from selenium.webdriver.support.ui import WebDriverWait
+        from selenium.webdriver.support import expected_conditions as EC
+        
+        options = webdriver.ChromeOptions()
+        options.add_argument('window-size=1200x600')
+        options.add_argument('headless')
+        driver = webdriver.Chrome(options=options)
+        driver.get('http://13.234.238.197:8000')
+        element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "terminal"))
+        )
+        terminal = driver.find_element_by_xpath("//div[@class='terminal']")
+        terminal.send_keys('ls' + Keys.ENTER)
+        terminal.send_keys('touch file.txt' + Keys.ENTER)
+        terminal.send_keys('git clone https://github.com/vedavidhbudimuri/ib-mini-projects' + Keys.ENTER)
+        terminal.send_keys('cat file.txt' + Keys.ENTER)
