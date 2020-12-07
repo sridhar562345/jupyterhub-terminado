6476632185f559694eb20b8defb21ff72dcdda1a
sridhar562345 <sridhar562345@gmail.com>
2020-12-07 17:57:00 +0530

    add script to install dependencies

--- loadtest/dependencies.sh
+++ loadtest/dependencies.sh
@@ -1,5 +1,9 @@
 sudo apt-get update
 
+sudo snap install chromium # takes a lot of time approx 15-20 min
+
+sudo apt-get install chromium-chromedriver
+
 sudo apt-get install python3-pip python3-venv
 
 python3 -m venv env
