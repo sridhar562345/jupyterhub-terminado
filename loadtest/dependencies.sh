c406af1858246414b22868b38ac7d658b8bf4d7c
sridhar562345 <sridhar562345@gmail.com>
2020-12-07 18:34:16 +0530

    add root permissions to dependencies script

--- loadtest/dependencies.sh
+++ loadtest/dependencies.sh
@@ -2,9 +2,7 @@ sudo apt-get update
 
 sudo snap install chromium # takes a lot of time approx 15-20 min
 
-sudo apt-get install chromium-chromedriver -y
-
-sudo apt-get install python3-pip python3-venv
+sudo apt-get install chromium-chromedriver python3-pip python3-venv -y
 
 python3 -m venv env
 
