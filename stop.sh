1aa77106876f696ba83392abc1ee237e175983de
sridhar562345 <sridhar562345@gmail.com>
2020-12-08 11:19:40 +0530

    modify stop.sh to remove stopped containers

--- stop.sh
+++ stop.sh
@@ -4,6 +4,6 @@ sudo docker-compose down
 # Kill containers of people who logged out without stopping their servers
 sudo docker kill $(sudo docker ps -q)
 
-## Tidy up everything
-sudo docker system prune --all
+## remove stopped containers
+sudo docker rm $(sudo docker ps -aq)
 
