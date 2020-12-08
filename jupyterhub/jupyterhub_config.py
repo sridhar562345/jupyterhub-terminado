ebc44a9f8f6d49d68a470e872a5fcab7b5e323b5
sridhar562345 <sridhar562345@gmail.com>
2020-12-08 17:06:33 +0530

    modify dockerfile to support ecsspawner

--- jupyterhub/jupyterhub_config.py
+++ jupyterhub/jupyterhub_config.py
@@ -20,17 +20,14 @@ c.JupyterHub.services = [
 c.JupyterHub.authenticator_class = tmpauthenticator.TmpAuthenticator
 # c.Authenticator.admin_users = {'root'}
 c.JupyterHub.spawner_class = 'awsspawner.EcsTaskSpawner'
-c.DockerSpawner.image = os.environ['DOCKER_NOTEBOOK_IMAGE']
-c.DockerSpawner.debug = True
-# c.Spawner.base_url = ''
 
 
 # Networking
 c.Spawner.strategy = 'ECSxEC2SpawnerHandler'
 c.Spawner.strategy_parms = {
-    'cluster_name': 'notebook-cluster',
+    'cluster_name': 'jupyterhub-terminado-cluster',
     'ec2_instance_template': 'jupyterhub-launch-template',
-    'ecs_task_definition': 'terminado',
+    'ecs_task_definition': 'terminado-task',
     'port': 8000
 }
 c.JupyterHub.hub_ip = 'jupyterhub'
